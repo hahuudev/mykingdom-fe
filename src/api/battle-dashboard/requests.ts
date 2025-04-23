8a75d91aff6c588758e41a849a9b394f74a19b1d
hahuudev <hahuu02dev@gmail.com>
2025-04-23 11:19:31 +0700

    feat: refactor code

--- src/api/battle-dashboard/requests.ts
+++ src/api/battle-dashboard/requests.ts
@@ -1,44 +1,5 @@
-import type { SetBattleSchema } from '@/components/BattleForm/libs/validators';
 import request from '../axios';
-import type { IBattle, IBattleQuery, IUploadFileResponse } from './types';
-
-export const getBattles = async (params: Partial<IBattleQuery>) => {
-  const { data } = await request({
-    url: '/admin-portal/battle',
-    method: 'GET',
-    params,
-  });
-
-  return data;
-};
-
-export const getBattleById = async (battleId: string) => {
-  const { data } = await request({
-    url: '/admin-portal/battle/' + battleId,
-    method: 'GET',
-  });
-
-  return data?.data;
-};
-
-export const createBattle = async (formData: SetBattleSchema): Promise<IBattle> => {
-  const { data } = await request({
-    url: '/admin-portal/battle/create',
-    method: 'POST',
-    data: formData,
-  });
-
-  return data?.data;
-};
-export const modifyBattle = async ({ formData, battleId }: { formData: SetBattleSchema; battleId: string }): Promise<IBattle> => {
-  const { data } = await request({
-    url: '/admin-portal/battle/update/' + battleId,
-    method: 'PUT',
-    data: formData,
-  });
-
-  return data?.data;
-};
+import type { IUploadFileResponse } from './types';
 
 export const uploadMultiFile = async (formData: FormData, onProgress?: (progress: number) => void): Promise<IUploadFileResponse[]> => {
   const { data } = await request({
--- src/api/comment/queries.ts
+++
@@ -1,8 +0,0 @@
-import { createQuery } from 'react-query-kit';
-import type { ICommentQuery, ICommentResponse } from './types';
-import { getListComments } from './requests';
-
-export const useListComments = createQuery<ICommentResponse, Partial<ICommentQuery>>({
-  queryKey: ['/api/comments'],
-  fetcher: (params) => getListComments(params),
-});
